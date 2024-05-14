19fb92917baf9a20dc1aeb1ceac8f4d892faea60
smilingsoumya1 <22053029@kiit.ac.in>
2024-05-14 11:19:51 +0530

    Add WhyChoose componen

---
+++ src/Components/WhyChoose/WhyChoose.js
@@ -0,0 +1,56 @@
+
+
+export default function WhyChoose() {
+    return (
+        <section className="">
+            <div>
+                <img src="" className=""></img>
+            </div>
+            <div>
+
+                <div className="flex m-2">
+                    <div className="flex">
+                        <div>
+                            <i className="fa fa-rocket"></i>
+                        </div>
+                        <div>
+                            <h3>Fast Delivery</h3>
+                            <p>Get your blood test delivered at home collect a sample from the your blood tests.</p>
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <i className="fa fa-rocket"></i>
+                        </div>
+                        <div>
+                            <h3>Fast Delivery</h3>
+                            <p>Get your blood test delivered at home collect a sample from the your blood tests.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="flex">
+                    <div className="flex">
+                        <div>
+                            <i className="fa fa-rocket"></i>
+                        </div>
+                        <div>
+                            <h3>Fast Delivery</h3>
+                            <p>Get your blood test delivered at home collect a sample from the your blood tests.</p>
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <i className="fa fa-rocket"></i>
+                        </div>
+                        <div>
+                            <h3>Fast Delivery</h3>
+                            <p>Get your blood test delivered at home collect a sample from the your blood tests.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div >
+        </section >
+    );
+}
