ce053d2f8a1f5023a39a2ab8b204babfb0ff6374
smilingsoumya1 <22053029@kiit.ac.in>
2024-05-14 12:57:46 +0530

    Update Navbar1 component styling and social media icons

--- src/Components/Navbar1/Navbar1.js
+++ src/Components/Navbar1/Navbar1.js
@@ -7,19 +7,18 @@ export default function Navbar1(){
 
 return(
 
-<section className="px-10  py-2 w-[100%] bg-green-500 text-white">
+<section className="px-20  w-[100%] bg-green-500 text-white flex justify-between">
     <div className="flex  ">
-   <div className=" mx-2 text-white  font-semibold  flex justify-center items-center text-center"><box-icon name='phone' color='white'  type='solid' animation='tada' ></box-icon><a className="text-sm  mx-1.5" href="tel:+9641208005"> 
+   <div className=" mx-2 text-white  font-semibold  flex justify-center items-center text-center"><box-icon name='phone' color='white'  type='solid' animation='tada' ></box-icon><a className="text-base  mx-1.5" href="tel:+9641208005"> 
 9641208005</a></div> 
-<div className=" mx-2 text-white  font-semibold flex justify-center items-center text-center"><box-icon name='mail-send' color='white' animation='flashing' flip='horizontal' ></box-icon><a className="text-sm  mx-1.5" href="mailto:recipient@example.com">kaews@gmail.com</a></div>
+<div className=" mx-2 text-white  font-semibold flex justify-center items-center text-center"><box-icon name='mail-send' color='white' animation='flashing' flip='horizontal' ></box-icon><a className="text-base  mx-1.5" href="mailto:recipient@example.com">kaews@gmail.com</a></div>
 
     </div>
-    <div>
-    <i class='bx bxl-facebook text-[24px] px-6 text-white  hover:bg-black bg-green-500 rounded-full p-2 transform hover:scale-110 transition-transform duration-500' ></i>            
-    <i class='bx bxl-facebook text-[24px] px-6 text-white  hover:bg-black bg-green-500 rounded-full p-2 transform hover:scale-110 transition-transform duration-500' ></i>            
-            <i class='bx bxl-facebook text-[24px] px-6 text-white  hover:bg-black bg-green-500 rounded-full p-2 transform hover:scale-110 transition-transform duration-500' ></i>            
-            <i class='bx bxl-facebook text-[24px] px-6 text-white  hover:bg-black bg-green-500 rounded-full p-2 transform hover:scale-110 transition-transform duration-500' ></i>            
-            <i class='bx bxl-facebook text-[24px] px-6 text-white  hover:bg-black bg-green-500 rounded-full p-2 transform hover:scale-110 transition-transform duration-500' ></i>            
+    <div className="flex">
+    <i class='bx bxl-instagram text-[26px] px-3 text-white  hover:text-black bg-green-500 rounded-full p-2 ' ></i>            
+    <i class='bx bxl-twitter text-[26px] px-3 text-white  hover:text-black bg-green-500 rounded-full p-2 ' ></i>            
+            <i class='bx bxl-wordpress text-[26px] px-3 text-white  hover:text-black bg-green-500 rounded-full p-2 ' ></i>            
+            <i class='bx bxl-linkedin text-[26px] px-3 text-white  hover:text-black bg-green-500 rounded-full p-2 ' ></i>            
 
     </div>
 </section>
