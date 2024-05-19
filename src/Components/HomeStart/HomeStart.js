5ba3871c722027a501f4565c5d09221eb0fb3a33
smilingsoumya1 <22053029@kiit.ac.in>
2024-05-19 17:35:35 +0530

    Add HomeStart component to Home page

---
+++ src/Components/HomeStart/HomeStart.js
@@ -0,0 +1,17 @@
+
+
+import Button from '@mui/material/Button';
+import bgImage from './BackgroundHome.svg';
+ export default function HomeStart(){
+    return (
+
+        <div className="relative bg-home bg-[#fff6a4]">
+        <img className="xl:h-screen w-full" src={bgImage} alt="Backgroundphoto" />
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center pb-60 justify-center">
+            <p className="text-[#d48e66] text-4xl m-16 font-bold ">We are the official KAEWS</p>
+            <Button variant="contained" >Know More</Button>
+
+        </div>
+    </div>
+    );
+ }
--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,6 +1,5 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import bgImage from "./BackgroundHome.svg"
+import HomeStart from "../../Components/HomeStart/HomeStart";
+
 import EnvironmentalSuggestion from "../../Components/SuggestionForGoodEnvironment/Suggestion";
 import GuildingPillars from '../../Components/GuidingPillars/GuidingPillars';
 import WhyChoose from '../../Components/WhyChoose/WhyChoose';
@@ -11,15 +10,8 @@ import BlogsForHome from '../../Components/BlogsForHomePage/BlogsHomePage';
 export default function Home() {
     return (
         <div className=" justify-center items-center ">
-            <div className="relative bg-home bg-[#fff6a4]">
-                <img className="xl:h-screen w-full" src={bgImage} alt="Backgroundphoto" />
-                <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center pb-60 justify-center">
-                    <p className="text-[#d48e66] text-4xl m-16 font-bold ">We are the official KAEWS</p>
-                    <Button variant="contained" >Know More</Button>
-
-                </div>
-            </div>
-
+           
+            <HomeStart />
             <div className='flex justify-center -mt-40'>  <GuildingPillars />
                 <GuildingPillars />
                 <GuildingPillars />
