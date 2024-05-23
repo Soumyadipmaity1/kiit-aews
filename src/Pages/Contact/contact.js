cfc9225330d6d899ff299beca57828196c7188db
smilingsoumya1 <22053029@kiit.ac.in>
2024-05-23 20:35:41 +0530

    Add ContactSymbol component to Contact page

--- src/Pages/Contact/contact.js
+++ src/Pages/Contact/contact.js
@@ -1,8 +1,12 @@
+import ContactSymbol from "../../Components/ContactSymbol/ContactSymbol";
+
+
+
 
 export default function Contact() {
     return (
-        <div className="contact-bg h-screen">
-            <h1>Contact</h1>
+        <div className="contact-bg h-screen flex justify-start">
+            <ContactSymbol />
         </div>
     );
 }
