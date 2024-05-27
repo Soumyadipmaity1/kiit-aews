aaacdf1c39e86b6d94f9c20e95782238c0cbd782
smilingsoumya1 <22053029@kiit.ac.in>
2024-05-27 15:02:06 +0530

    Update Blog page header styling for better responsiveness

--- src/Pages/Blog/blog.js
+++ src/Pages/Blog/blog.js
@@ -5,16 +5,14 @@ export default function Blog() {
         <div>
             <div className=' mx-auto'>
                 <img className='w-[100%] ' src={Blogimg} alt='' />
-                <h1 className='text-6xl text-center   -mt-[510px] font-bold   pb-6'>Student Blogs</h1>
+                <h1 className='text-6xl text-center   xl:-mt-[490px] 2lg:-mt-[435px] font-bold   pb-6'>Student Blogs</h1>
                 </div>
                 <div className="px-24 py-10 pb-40">
                 <h1 className='text-4xl text-center   mx-auto font-semibold mt-[480px]  pb-6'>Latest Blogs Post</h1>
                 <div className='w-60 mx-auto border-green-500 mb-14 border-[3.5px] rounded'></div>
 
                 <BlogsForHome />
-                <BlogsForHome />
-
-                <BlogsForHome />
+           
 
 
             </div>
