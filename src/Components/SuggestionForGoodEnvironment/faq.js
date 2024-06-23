e3fac95554f4f5b17d6010bdc0c5993050c24a5f
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-23 22:50:00 +0530

    refactor: Remove unused code and optimize FAQ component
    This commit removes the unused code and optimizes the FAQ component by removing the unnecessary background color and image. The code cleanup improves the performance and readability of the component.

--- src/Components/SuggestionForGoodEnvironment/faq.js
+++ src/Components/SuggestionForGoodEnvironment/faq.js
@@ -4,16 +4,10 @@ import EnvironmentalSuggestion from "./Suggestion";
 
 export default function FAQ() {
     return (
-        <div className="py-24 bg-green-200">
-            <h1 className="text-4xl font-bold text-center mb-20">Frequently Asked Questions</h1>
-            <div className="flex justify-center ">
-            <div className="w-5/12 px-10">
+        <div className=" ">
+         
                 <EnvironmentalSuggestion />
-            </div>
-            <div className="w-5/12 px-10 rounded-full">
-                <img className="" src="https://images.unsplash.com/photo-1611003229186-80e40cd54966?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJhYnklMjBhbmltYWxzfGVufDB8fDB8fHww" alt="" />
-            </div>
-            </div>
+        
         </div>
     )
 }
