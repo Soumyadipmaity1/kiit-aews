dd7d10f32981bc57cad22b8a4b4e527bafcfca5d
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-23 22:54:14 +0530

    feat: Update WhyChoose component with new content
    This commit updates the WhyChoose component by replacing the existing content with new information about the KIIT AEWS organization. The changes include updating the text, adding new headings, and modifying the color scheme. These updates enhance the presentation of the component and provide a more engaging user experience.

--- src/Components/WhyChoose/WhyChoose.js
+++ src/Components/WhyChoose/WhyChoose.js
@@ -1,10 +1,10 @@
-import ChooseUsimage from './dog-1728494.svg'
+const ChooseUsimage = 'https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?cs=srgb&dl=pexels-pixabay-47547.jpg&fm=jpg'
 
 export default function WhyChoose() {
     return (
-        <section className="flex h-[700px]">
+        <section className="flex mt-20">
             
-            <div className="w-7/12 my-auto">
+            <div className="w-my-auto text-justify ">
 
                 <div className="flex mb-5 ">
                     <div className="flex w-1/2  m-4 mx-4">
@@ -12,8 +12,11 @@ export default function WhyChoose() {
                             <i class='bx bxs-first-aid text-[45px]   '></i>
                         </div>
                         <div>
-                            <h3 className="py-1 text-[22px] font-bold">Fast Delivery</h3>
-                            <p className="text-gray-100">Get your blood test delivered at home collect a sample from the your blood tests.</p>
+                            <h3 className="py-1 text-[22px] text-[#00ff6a]  font-bold">Platform for a noble cause</h3>
+                            <p className="text-gray-100">
+                            Join KIIT AEWS and become a powerful voice for environment and the animals. Contribute fresh ideas, actively participate in impactful projects, seminars, workshops, campaigns, etc and be part of shaping a more sustainable tomorrow.
+
+                            </p>
                         </div>
                     </div>
                     <div className="flex w-1/2 m-4 mx-4">
@@ -21,8 +24,12 @@ export default function WhyChoose() {
                             <i class='bx bxs-first-aid text-[45px]   '></i>
                         </div>
                         <div>
-                            <h3 className="py-1 text-[22px] font-bold">Fast Delivery</h3>
-                            <p className="text-gray-100">Get your blood test delivered at home collect a sample from the your blood tests.</p>
+                            <h3 className="py-1 text-[22px] text-[#00ff6a]  font-bold">Fast Delivery</h3>
+                            <p className="text-gray-100">
+                            Enjoy a vibrant community atmosphere with fun activities, games, social gatherings, and bonding experiences with fellow members. KIIT AEWS isn't just about hard work! We offer engaging activities, informative events, and a supportive community that makes a difference while having fun.
+
+
+                            </p>
                         </div>
                     </div>
 
@@ -33,8 +40,11 @@ export default function WhyChoose() {
                             <i class='bx bxs-first-aid text-[45px]   '></i>
                         </div>
                         <div>
-                            <h3 className="py-1 text-[22px] font-bold">Fast Delivery</h3>
-                            <p className="text-gray-100">Get your blood test delivered at home collect a sample from the your blood tests.</p>
+                            <h3 className="py-1 text-[22px] text-[#00ff6a]  font-bold">Fast Delivery</h3>
+                            <p className="text-gray-100">
+                            Develop leadership skills and hone valuable skills like communication, teamwork, problem-solving, etc through collaborative projects in a supportive environment. Everyone's ideas are welcome here!. We offer opportunities in both technical and non-technical departments (webdev, graphic design, R&D, video editing, etc). Our highly active community fosters inclusivity, where you can contribute fresh ideas, actively participate in meaningful projects and be part of shaping a more sustainable tomorrow.
+
+                            </p>
                         </div>
                     </div>
                     <div className="flex w-1/2 m-4 mx-4">
@@ -42,16 +52,18 @@ export default function WhyChoose() {
                             <i class='bx bxs-first-aid text-[45px]   '></i>
                         </div>
                         <div>
-                            <h3 className="py-1 text-[22px] font-bold">Fast Delivery</h3>
-                            <p className="text-gray-100">Get your blood test delivered at home collect a sample from the your blood tests.</p>
+                            <h3 className="py-1 text-[22px]  text-[#00ff6a] font-bold">Fast Delivery</h3>
+                            <p className="text-gray-100">
+                            Connect with like-minded individuals, professionals and NGOs in the field of animal welfare and environmental conservation. Enhance your resume with practical experience in project management, event planning, fundraising, or other relevant areas. Active participation strengthens your resume and demonstrates your commitment to causes beyond academics.
+                                </p>
                         </div>
                     </div>
 
                 </div>
             </div >
-            <div className=" w-1/2 ">
+            {/* <div className=" w-4/12 mt-20">
                 <img className='' src={ChooseUsimage} ></img>
-            </div>
+            </div> */}
         </section >
     );
 }
