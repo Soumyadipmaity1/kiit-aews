8c41e538a7a3e0c12deff6aaae58a6965cf754eb
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-28 00:08:56 +0530

    refactor: Update AboutUs page layout and components

--- src/Pages/AboutUs/about.js
+++ src/Pages/AboutUs/about.js
@@ -1,9 +1,20 @@
-
-
+import AboutUsStart from "../../Components/AboutUsComponent/AboutUsStart";
+import OurMissonVison from "../../Components/AboutUsComponent/OurMisonVison";
 export default function About() {
-    return (
-        <div>
-            <h1>About Us</h1>
+  return (
+    <div>
+      <section>
+        <div className=" AboutStartBg text">
+          {" "}
+            {/* <h1 className=" font-bold text pt-[23vh] text-yellow-400 font-serif text-center">
+                About Us </h1> */}
+          <h1 className="text-white text-6xl font-bold pt-8 text font-serif text-center">Who We Are</h1>
         </div>
-    );
+
+        <AboutUsStart />
+        <OurMissonVison />
+
+      </section>
+    </div>
+  );
 }
