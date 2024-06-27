020df8a78802a85d813dc8edacac5bb8a5865288
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-28 00:09:05 +0530

    refactor: Update Blog page layout and styling for better readability

--- src/Pages/Blog/blog.js
+++ src/Pages/Blog/blog.js
@@ -3,12 +3,16 @@ import Blogimg from './blogstart4.svg'
 export default function Blog() {
     return (
         <div>
-            <div className=' mx-auto'>
-                <img className='w-[100%] ' src={Blogimg} alt='' />
-                <h1 className='text-6xl text-center   xl:-mt-[490px] 2lg:-mt-[435px] font-bold   pb-6'>Student Blogs</h1>
+            <div className=' mx-auto BlogStartBg'>
+                
+                <h1 className='text-6xl text-center font-bold font-serif text-white textBlog pt-[18vh]'>Student Blogs</h1>
+                <h2 className='text-white text-center text-md textBlog mt-10 font-sans'>
+                <a href="/">Home</a> &gt; <a href="/blogs">Blog</a>
+
+                </h2>
                 </div>
-                <div className="px-24 py-10 pb-40">
-                <h1 className='text-4xl text-center   mx-auto font-semibold mt-[480px]  pb-6'>Latest Blogs Post</h1>
+                <div className="py-10">
+                <h1 className='text-4xl text-center   mx-auto font-semibold   pb-6'>Latest Blogs Post</h1>
                 <div className='w-60 mx-auto border-green-500 mb-14 border-[3.5px] rounded'></div>
 
                 <BlogsForHome />
