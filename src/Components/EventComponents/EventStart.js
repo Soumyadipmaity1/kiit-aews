9d593c146a3c73b946a3b3f3ffd3aae3219eae49
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-28 00:07:53 +0530

    refactor: Add EventStart component for displaying event start page

---
+++ src/Components/EventComponents/EventStart.js
@@ -0,0 +1,17 @@
+
+
+export default function EventStart(){
+
+
+    return(
+        <div className=" EventStartBg text">
+        {" "}
+          
+        <h1 className="text-white text-6xl textEvent  font-bold pt-[20vh] text font-serif text-center">E v e n t s</h1>
+        <h2 className='text-white text-center textEvent t mt-10 font-sans'>
+                <a href="/">Home</a> &gt; <a href="/events">Events</a>
+
+                </h2>
+      </div>
+    );
+}
