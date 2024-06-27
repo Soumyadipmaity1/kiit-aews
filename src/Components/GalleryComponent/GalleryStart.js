64ac985ba1a0706f2fd89e697a25564facee879e
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-28 00:08:16 +0530

    refactor: Add GalleryStart component for displaying gallery start page

---
+++ src/Components/GalleryComponent/GalleryStart.js
@@ -0,0 +1,17 @@
+
+
+export default function GalleryStart(){
+
+
+    return(
+        <div className=" GalleryStartBg text">
+        {" "}
+          
+        <h1 className="text-white text-6xl textGallery  font-bold pt-[28vh] text font-serif text-center">Photo Gallery</h1>
+        <h2 className='text-white font-semibold text-center textGallery t mt-10 font-sans'>
+                <a href="/">Home</a> &gt; <a href="/gallery">Gallery</a>
+
+                </h2>
+      </div>
+    );
+}
