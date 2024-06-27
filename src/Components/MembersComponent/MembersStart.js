151c29b20b172b682fdd7e887f0822a22fd8702d
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-28 00:08:24 +0530

    feat: Add MemberStart component for displaying member start page

---
+++ src/Components/MembersComponent/MembersStart.js
@@ -0,0 +1,17 @@
+
+
+export default function MemberStart(){
+
+
+    return(
+        <div className=" MemberStartBg text">
+        {" "}
+          
+        <h1 className="text-white text-6xl textMember  font-bold pt-[22vh] text font-serif text-center">Meet Our <span className="text-[#4dff0c]">Team</span></h1>
+        <h2 className='text-white text-center textMember t mt-10 font-sans'>
+                <a href="/">Home</a> &gt; <a href="/members">Member</a>
+
+                </h2>
+      </div>
+    );
+}
