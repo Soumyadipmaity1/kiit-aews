ef0b7298612cf7e6d711629bf796b5718d635c6b
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-28 00:09:54 +0530

    refactor: Add MemberStart component to Member page for displaying member start page

--- src/Pages/Members/member.js
+++ src/Pages/Members/member.js
@@ -1,11 +1,19 @@
 
 // import MemberCards from "../../Components/Membercards/MemberCards";
+import MemberStart from "../../Components/MembersComponent/MembersStart";
 import TabSwitchingComponent from "../../Components/TabSwitching/TabSwitching";
+
  export default function Member() {
     return (
-        <div className="p-10 mx-auto flex justify-center">
+
+
+        <div>
+            <MemberStart/>
+               <div className="p-10 mx-auto flex justify-center">
             <TabSwitchingComponent />
 
         </div>
+        </div>
+     
     );
 }
