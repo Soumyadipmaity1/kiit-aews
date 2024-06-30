0910213118946c4484b94c4d44ab176530993cd1
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-30 13:02:25 +0530

    refactor: Update AboutUsStart button text for better clarity

--- src/Components/AboutUsComponent/AboutUsStart.js
+++ src/Components/AboutUsComponent/AboutUsStart.js
@@ -14,7 +14,7 @@ export default function AboutUsStart () {
            <div className='w-[60%]  px-10 '>
            <h1 className="text-4xl font-bold text-white mb-8">About <span className='text-[#61e045]'>KAEWS</span></h1>
             <p className="text-md text-gray-300  text-justify ">The official KIIT Animal & Environment Welfare Society works hard to keep our environment safe for us as well as for the animals. The main motto of this Society is “Pause for a Cause”. The Society is a group of enthusiastic and experienced people who have great affection towards our Animal friends and Environment. Throughout the year they organize various events related to Animal Rights & Welfare and Environmental Causes. Apart from events, KIIT-AEWS’s Volunteer Wing looks after injured and needy animals in and around Bhubaneswar City in Odisha, India.</p>
-     <button className="bg-[#61e045] hover:bg-[#9aef86] text-black font-semibold text-lg px-8 py-2 mt-8 rounded-full">Want to Contribute ?</button>
+     <button className="bg-[#61e045] hover:bg-[#9aef86] text-black font-semibold text-lg px-8 py-2 mt-8 rounded-full">Let's Contribute</button>
            </div>
            <div>
 <img src={img} alt="image" className="w-full h-auto rounded-lg shadow-lg mt-6 " />'
