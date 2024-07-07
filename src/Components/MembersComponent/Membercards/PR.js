7f5d292468f83a79f7a9633c6a0c8d70f3ddcf39
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-07 11:41:33 +0530

    refactor: Update PR member cards layout and styling

--- src/Components/MembersComponent/Membercards/PR.js
+++ src/Components/MembersComponent/Membercards/PR.js
@@ -5,24 +5,150 @@ import { FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa';
 import Image from './photo_2024-05-14_17-42-08.jpg';
 
 const memberData = [
-    {
-     
-        title: "Soumyadip Maity",
-        imgSrc: Image,
-        social: {
-          instagram: "https://instagram.com/soumyadip",
-          linkedin: "https://linkedin.com/in/soumyadip",
-          twitter: "https://twiter.com/soumyadip"
-      }
-    },
-
+  {
+    title: "Alkesh Bhati",
+    imgSrc: "./AlkeshBhati_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Tanya Sharma",
+    imgSrc: "./TanyaSharma _PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Ansuman Mishra",
+    imgSrc: "./ANSUMAN_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sneha Fatehramka",
+    imgSrc: "./SnehaFatehramka _PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Areen Thakur",
+    imgSrc: "./AreenThakur_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Arpita Pal",
+    imgSrc: "./ArpitaPal_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Guttal Gayatri",
+    imgSrc: "./Guttalagayatri_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sai Sanjana Dwarapudi",
+    imgSrc: "./SaiSanjanaDwarapudi_PR.jpeg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Himanshu Kumar",
+    imgSrc: "./HimanshuKumar_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Ruditha",
+    imgSrc: "./Ruditha_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Kunal Saw",
+    imgSrc: "./KunalSaw_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Manaswini Mohapatra",
+    imgSrc: "./manaswinimohapatra_pr.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "NIKHIL RAJ SINHA",
+    imgSrc: "./NIKHILRAJSINHA_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Vaibhavi",
+    imgSrc: "./Vaibhavi_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Rohit Kumar Satpathy",
+    imgSrc: "./RohitKumarSatpathy_PR.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  
 ];
 
+
 const MemberPost = ({ title, imgSrc, social }) => (
-  <div className="w-60 m-3  h-auto members  bg-slate-white shadow-md rounded-md overflow-hidden group">
+  <div className="w-64 m-3  h-auto members  bg-slate-white shadow-md rounded-md overflow-hidden group">
     <div className="relative rounded-t-md image overflow-hidden">
       <img
-        className="w-full rounded-t-md h-76 transition-transform duration-300 transform group-hover:scale-110"
+        className="w-full rounded-t-md  transition-transform duration-300 transform group-hover:scale-110"
         src={imgSrc}
         alt="leadImage"
       />
@@ -38,7 +164,7 @@ const MemberPost = ({ title, imgSrc, social }) => (
           <FaTwitter size={25} />
         </a>
       </div>
-      <div className="absolute inset-0 flex mt-[205px] py-1 bg-green-500 justify-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+      <div className="absolute inset-0 flex mt-[222px] py-1 bg-green-500 justify-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
         <h3 className="text-center font-bold text-[15px]  text-white">{title}</h3>
       </div>
     </div>
