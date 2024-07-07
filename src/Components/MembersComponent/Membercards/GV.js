769c94fe6506bf35200fdf723c2094a57fb809c3
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-07 11:36:33 +0530

    refactor: Update MemberGvCard component

--- src/Components/MembersComponent/Membercards/GV.js
+++ src/Components/MembersComponent/Membercards/GV.js
@@ -2,21 +2,451 @@
 
 import React from 'react';
 import { FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa';
-import Image from './photo_2024-05-14_17-42-08.jpg';
 
 const memberData = [
-    {
-     
-        title: "Soumyadip Maity",
-        imgSrc: Image,
-        social: {
-          instagram: "https://instagram.com/soumyadip",
-          linkedin: "https://linkedin.com/in/soumyadip",
-          twitter: "https://twiter.com/soumyadip"
-      }
-    },
+  {
+    title: "Aaditya Aanand",
+    imgSrc: "./gvimage/AadityaAanand.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Abhinandan Singh",
+    imgSrc: "./gvimage/Abhinandansingh_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Aditya Kumar Singh",
+    imgSrc: "./gvimage/AdityaKumarSingh_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Aman Jagannath",
+    imgSrc: "./gvimage/AmanJagannath_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Anchita Patro",
+    imgSrc: "./gvimage/Anchita Patro_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Ankit Kumar",
+    imgSrc: "./gvimage/Ankit Kumar_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Anuj Pandey",
+    imgSrc: "./gvimage/AnujPandey_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Ashish Jha",
+    imgSrc: "./gvimage/AshishJha_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Ashmita Kar",
+    imgSrc: "./gvimage/Ashmita Kar_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Avinash Kumar",
+    imgSrc: "./gvimage/Avinash Kumar_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Biswajit Patel",
+    imgSrc: "./gvimage/BiswajitPatel_gv.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Debadrita Ghosh",
+    imgSrc: "./gvimage/DebadritaGhosh_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Gagan Visen",
+    imgSrc: "./gvimage/GaganVisen_GV.png",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Harshit",
+    imgSrc: "./gvimage/Harshit_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Indrayudh Mukherjee",
+    imgSrc: "./gvimage/IndrayudhMukherjee_GV.jpeg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Isha Jaswal",
+    imgSrc: "./gvimage/IshaJaswal_gv.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Khusi Saxena",
+    imgSrc: "./gvimage/KhusiSaxena_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Kirti Saxena",
+    imgSrc: "./gvimage/KirtiSaxena_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Krishna Sreekar Upadhayayula",
+    imgSrc: "./gvimage/KrishnaSreekarUpadhayayula_GV.png",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Kritika Roy",
+    imgSrc: "./gvimage/KritikaRoy_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Mandal Soren",
+    imgSrc: "./gvimage/mandalsoren_gv.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Manya Shri Tripathi",
+    imgSrc: "./gvimage/ManyaShriTripathi_gv.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Medha Saha",
+    imgSrc: "./gvimage/MedhaSaha_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Naibedya Mohanty",
+    imgSrc: "./gvimage/NAIBEDYA_MOHANTY_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Pranav Bharti",
+    imgSrc: "./gvimage/PRANAVBHARTI_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Prapti Chaudhry",
+    imgSrc: "./gvimage/Prapti Chaudhry_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Pratigya Kumari",
+    imgSrc: "./gvimage/Pratigya_Kumari_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Priyanshi",
+    imgSrc: "./gvimage/PRIYANSHI_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Ritika Dey",
+    imgSrc: "./gvimage/RitikaDey_gv.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sagarika",
+    imgSrc: "./gvimage/Sagarika_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "SAGARIKA MOHANTY",
+    imgSrc: "./gvimage/SAGARIKA-MOHANTY_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sahil Kumar",
+    imgSrc: "./gvimage/Sahil_Kumar_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sahil Behera",
+    imgSrc: "./gvimage/SahilBehera_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sandesh Kumar",
+    imgSrc: "./gvimage/SandeshKumar_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sapna Sharma",
+    imgSrc: "./gvimage/SapnaSharma_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sasank Agarwal",
+    imgSrc: "./gvimage/SasankAgarwal_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sayantan Panda",
+    imgSrc: "./gvimage/SayantanPanda_gv.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Shambhavi Chaudhary",
+    imgSrc: "./gvimage/ShambhaviChaudhary_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Shiven Sisodia",
+    imgSrc: "./gvimage/ShivenSisodia_gv.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Shivranjanee Bhattarcharya",
+    imgSrc: "./gvimage/ShivranjaneeBhattacharya_gv.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Shreya Allupati",
+    imgSrc: "./gvimage/ShreyaAllupati_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sidharth Sethia",
+    imgSrc: "./gvimage/SidharthSethia_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Simran Arya",
+    imgSrc: "./gvimage/Simran arya_gv.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Smriti Chowdhury",
+    imgSrc: "./gvimage/Smriti Chowdhury_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Sreeja Dash",
+    imgSrc: "./gvimage/SreejaDash_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Srinjoyee Ray",
+    imgSrc: "./gvimage/SrinjoyeeRay_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Subha Darshini",
+    imgSrc: "./gvimage/Subha_Darshini_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Subhadra Khuntia",
+    imgSrc: "./gvimage/SubhadraKhuntia_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
+  {
+    title: "Swati Priyadarshini",
+    imgSrc: "./gvimage/Swati Priyadarshini_GV.jpg",
+    social: {
+      instagram: "https://instagram.com/",
+      linkedin: "https://linkedin.com/in/",
+      twitter: "https://twitter.com/"
+    }
+  },
 
-];
+]
 
 const MemberPost = ({ title, imgSrc, social }) => (
   <div className="w-60 m-3  h-auto members  bg-slate-white shadow-md rounded-md overflow-hidden group">
