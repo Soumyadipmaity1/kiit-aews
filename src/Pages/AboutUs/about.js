413d171194d551b7ef18c3be9008c16737cf5ae7
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-07 11:43:32 +0530

    refactor: Update AboutUs page layout and styling

--- src/Pages/AboutUs/about.js
+++ src/Pages/AboutUs/about.js
@@ -4,11 +4,11 @@ export default function About() {
   return (
     <div>
       <section>
-        <div className=" AboutStartBg text">
+        <div className=" AboutStartBg text justify-center flex items-center">
           {" "}
             {/* <h1 className=" font-bold text pt-[23vh] text-yellow-400 font-serif text-center">
                 About Us </h1> */}
-          <h1 className="text-white text-6xl font-bold pt-8 text font-serif text-center">Who We Are</h1>
+          <h1 className="text-white text-6xl font-bold text font-serif text-center">Who We Are</h1>
         </div>
 
         <AboutUsStart />
