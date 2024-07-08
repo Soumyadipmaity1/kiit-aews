7406d842277af2c61da2b92659e1a1b488ffeb54
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-09 01:37:55 +0530

    refactor: Update Home page with responsive layout for HomeStart component

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -12,11 +12,13 @@ export default function Home() {
     return (
         <div className=" justify-center items-center ">
            
-            {/* <H/omeStart /> */}
+         <div className='hidden lg:block'>
+         <HomeStart />
             {/* <div className='flex justify-center -mt-40'>  <GuildingPillars />
                 <GuildingPillars />
                 <GuildingPillars />
             </div> */}
+         </div>
             <AnalysisCount />
             <div className='py-20  bg-[#11463d] lg:px-10 2lg:px-   '>
                 <h1 className='text-4xl font-bold text-white mx-auto text-center mb-12'>Why Choose Us?</h1>
