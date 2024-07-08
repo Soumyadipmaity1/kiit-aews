10a99aa05cd4818034628d5254c8d50a82113c40
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-08 14:45:44 +0530

    refactor: Update Blog component to improve styling and structure

--- src/Pages/Blog/blog.js
+++ src/Pages/Blog/blog.js
@@ -1,21 +1,20 @@
-import BlogsForHome from "../../Components/HomeComponent/BlogsForHomePage/BlogsHomePage"
-// import Blogimg from './blogstart4.svg'
+import BlogsContent from "../../Components/BlogComponent/BlogsContent";
 export default function Blog() {
     return (
         <div>
             <div className=' mx-auto BlogStartBg'>
                 
-                <h1 className='text-6xl text-center font-bold font-serif text-white textBlog pt-[18vh]'>Student Blogs</h1>
-                <h2 className='text-white text-center text-md textBlog mt-10 font-sans'>
+                <h1 className='sm:text-6xl text-5xl  text-center font-bold font-serif flex items-center justify-center text-white textBlog pt-[18vh]'>Student Blogs</h1>
+                {/* <h2 className='text-white text-center text-md textBlog mt-10 font-sans'>
                 <a href="/">Home</a> &gt; <a href="/blogs">Blog</a>
 
-                </h2>
+                </h2> */}
                 </div>
                 <div className="py-10">
-                <h1 className='text-4xl text-center   mx-auto font-semibold   pb-6'>Latest Blogs Post</h1>
-                <div className='w-60 mx-auto border-green-500 mb-14 border-[3.5px] rounded'></div>
+                <h1 className='lg:text-4xl text-3xl text-center   mx-auto font-semibold  pb-4 lg:pb-6'>Latest Blogs Post</h1>
+                <div className='lg:w-60 w-52 border-[2px] mx-auto border-green-500 mb-14 lg:border-[3.5px] rounded'></div>
 
-                <BlogsForHome />
+               <BlogsContent />
            
 
 
