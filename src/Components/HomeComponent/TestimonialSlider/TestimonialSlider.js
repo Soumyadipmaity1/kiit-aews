c05bf82c13adfe875e85fc451bdf1670401f2b46
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-08 16:15:41 +0530

    refactor: Update TestimonialSlider component with responsive design and layout improvements

--- src/Components/HomeComponent/TestimonialSlider/TestimonialSlider.js
+++ src/Components/HomeComponent/TestimonialSlider/TestimonialSlider.js
@@ -1,54 +1,99 @@
-
-import Slider from 'react-slick';
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import TestimonialCard from './TestimonialCard';
+import TestimonialCard from "./TestimonialCard";
 
 const TestimonialSlider = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 3000, 
+    speed: 3000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true, 
-    autoplaySpeed: 4000, 
-    arrows:false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
     pauseOnHover: true,
   };
 
   return (
-    <div className='p-10 pb-28 py-20 bg-white '>
-      <h2 className=' text-center text-black text-4xl font-bold mb-4  '> Testimonial </h2>
-      <div className='w-40 mx-auto border-green-500 mb-14 border-[3.5px] rounded'></div>
-      
-      <Slider {...settings}>
-       
-        <div >
-        <div className='flex justify-center'>
-        <TestimonialCard cardType="card1" />
-        <TestimonialCard cardType="card2" />
-        </div>
-        </div>
+    <>
+      <div className="p-10 pb-28 hidden lg:block py-20 bg-white ">
+        <h2 className=" text-center text-black text-4xl font-bold mb-4  ">
+          {" "}
+          Testimonial{" "}
+        </h2>
+        <div className="w-40 mx-auto border-green-500 mb-14 border-[3.5px] rounded"></div>
 
-        <div >
-        <div className='flex justify-center'>
-        <TestimonialCard cardType="card3" />
-        <TestimonialCard cardType="card4" />
-        </div>
-        </div>
-        
-        <div >
+        <Slider {...settings}>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card1" />
+              <TestimonialCard cardType="card2" />
+            </div>
+          </div>
 
-            <div className='flex justify-center'>
-        <TestimonialCard cardType="card5" />
-        <TestimonialCard cardType="card6" />
-        </div>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card3" />
+              <TestimonialCard cardType="card4" />
+            </div>
+          </div>
 
-        </div>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card5" />
+              <TestimonialCard cardType="card6" />
+            </div>
+          </div>
+        </Slider>
+      </div>
+      <div className="p-5 pb-28 lg:hidden block py-20 bg-white ">
+        <h2 className=" text-center text-black text-4xl font-bold mb-4  ">
+          {" "}
+          Testimonial{" "}
+        </h2>
+        <div className="w-40 mx-auto border-green-500 mb-14 border-[3.5px] rounded"></div>
 
-      </Slider>
-    </div>
+        <Slider {...settings}>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card1" />
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card2" />
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card3" />
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card4" />
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card5" />
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card6" />
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <TestimonialCard cardType="card1" />
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </>
   );
 };
 
