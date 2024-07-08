f92c6f5b98ff51c5563563b00c969f819f05d157
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-08 14:45:00 +0530

    refactor: Update EventImages component to use full width and add border to image1

--- src/Components/EventComponents/EventCards/EventImages.js
+++ src/Components/EventComponents/EventCards/EventImages.js
@@ -1,11 +1,11 @@
 
 const EventImages = ({ image1Src, image2Src }) => {
     return (
-        <div className=" flex  p-5">
-            <img className="w-1/2 mr-4 rounded-3xl" src={image1Src} alt="" />
-            <img className="w-1/2  rounded-3xl" src={image2Src} alt="" />
+        <div className="lg:w-2/5 flex  p-5">
+            <img className="w-full mr-4 border-green-500 border-4 rounded-3xl" src={image1Src} alt="" />
+            {/* <img className="w-1/2  rounded-3xl" src={image2Src} alt="" /> */}
         </div>
     );
-};
+}
 
 export default EventImages;
