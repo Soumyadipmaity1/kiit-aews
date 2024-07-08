ac7705ff285542a5ed4c9ad9578ee12c2df45f8e
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-08 14:44:47 +0530

    refactor: Update EventStart component styling for better visual consistency

--- src/Components/EventComponents/EventStart.js
+++ src/Components/EventComponents/EventStart.js
@@ -4,14 +4,14 @@ export default function EventStart(){
 
 
     return(
-        <div className=" EventStartBg text">
+        <div className=" EventStartBg justify-center flex items-center ">
         {" "}
           
-        <h1 className="text-white text-6xl textEvent  font-bold pt-[20vh] text font-serif text-center">E v e n t s</h1>
-        <h2 className='text-white text-center textEvent t mt-10 font-sans'>
+        <h1 className="text-white lg:text-6xl text-5xl textEvent  font-bold  text font-serif text-center">E v e n t s</h1>
+        {/* <h2 className='text-white text-center textEvent t mt-10 font-sans'>
                 <a href="/">Home</a> &gt; <a href="/events">Events</a>
 
-                </h2>
+                </h2> */}
       </div>
     );
 }
