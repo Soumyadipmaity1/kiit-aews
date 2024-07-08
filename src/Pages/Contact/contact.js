432ee6d5bafa6b023e5bad72ce4bdc5e73b6f706
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-08 14:45:57 +0530

    refactor: Update Contact page layout for responsive design

--- src/Pages/Contact/contact.js
+++ src/Pages/Contact/contact.js
@@ -6,7 +6,7 @@ import GetInTouch from "../../Components/ContactComponent/GetInTouch";
 
 export default function Contact() {
     return (
-        <div className=" pb-16 lg:pt-5 flex justify-center px-6 lg:px-20">
+        <div className=" pb-16 lg:pt-5 flex justify-center px-6 lg:px-0 2lg:px-10 xl:px-20">
 <GetInTouch/>
         </div>
     );
