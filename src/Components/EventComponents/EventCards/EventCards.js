4e8102f0f7c8e208758d7d3cdcb05fd36ace21c3
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-08 14:44:53 +0530

    refactor: Update EventCards component to include motive prop and improve styling

--- src/Components/EventComponents/EventCards/EventCards.js
+++ src/Components/EventComponents/EventCards/EventCards.js
@@ -1,12 +1,14 @@
-const EventCard = ({ title, date, details }) => {
+const EventCard = ({ title, date, details, motive }) => {
     return (
-      <div className="  p-4  ">
-        <ul className="flex   p-5 items-end justify-between">
-        <h1 className="text-4xl px-5 text font-bold text-center  text-white p-2 ">{title}</h1>
+      <div className="lg:w-3/5  p-0 sm:p-4  ">
+        <ul className="lg:flex   items-end justify-between">
+        <h1 className="text-4xl text font-bold text-center  text-white py-2 ">{title}</h1>
 
           <li className="font-bold text-center  p-2 text-gray-400">{date}</li>
  </ul>
-        <p className="p-5 text-yellow-200">{details}</p>
+ <p className=" py-3 text-xl text-center lg:text-start font-semibold text-yellow-400">{motive}</p>
+
+        <p className=" text-justify text-slate-200">{details}</p>
       </div>
     );
   };
