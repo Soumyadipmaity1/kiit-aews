c57fc57d826a4a1b91b15cea0e82b8c3999c5e14
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-09 18:41:49 +0530

    refactor: Update Gallery component to include Paritantra event slider

--- src/Pages/Gallery/gallery.js
+++ src/Pages/Gallery/gallery.js
@@ -1,10 +1,18 @@
- import GalleryStart from "../../Components/GalleryComponent/GalleryStart";
+ import Paritantra from "../../Components/EventComponents/EventCards/EventSlider/Paritantra/Paritantra";
+import GalleryStart from "../../Components/GalleryComponent/GalleryStart";
 import PhotoPopUpImages from "../../Components/PhotoPopup/Photopopup";
 
  export default function Gallery() {
     return (
         <div>
             <GalleryStart/>
+            <div className="justify-center flex flex-wrap m-2 my-12 ">
+      <Paritantra/>
+      <Paritantra/>
+      <Paritantra/>
+      <Paritantra/>
+
+      </div>
             <PhotoPopUpImages />
         </div>
     );
