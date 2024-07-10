0c5797a334b033fe3e9dc36aa9a2489d992780b2
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-11 05:29:28 +0530

    refactor: Update AboutUs page with lazy loading for images

--- src/Pages/AboutUs/about.js
+++ src/Pages/AboutUs/about.js
@@ -4,13 +4,18 @@ export default function About() {
   return (
     <div>
       <section>
-        <div className=" AboutStartBg text justify-center flex items-center">
+        <div className="hidden AboutStartBg  justify-center lg:flex items-center">
           {" "}
             {/* <h1 className=" font-bold text pt-[23vh] text-yellow-400 font-serif text-center">
                 About Us </h1> */}
           <h1 className="text-white lg:text-6xl text-5xl font-bold text font-serif text-center">Who We Are</h1>
         </div>
-
+        <div className="AboutMobile  h-[330px] lg:hidden text justify-center flex items-center">
+          {" "}
+            {/* <h1 className=" font-bold text pt-[23vh] text-yellow-400 font-serif text-center">
+                About Us </h1> */}
+          <h1 className="text-white lg:text-6xl text-5xl font-bold text font-serif text-center">Who We Are</h1>
+        </div>
         <AboutUsStart />
         <OurMissonVison />
 
