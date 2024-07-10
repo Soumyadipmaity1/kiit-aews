00440eb278005daac63004eb0cab487074871bd8
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-11 05:28:39 +0530

    refactor: Update MembersStart component for responsive design

--- src/Components/MembersComponent/MembersStart.js
+++ src/Components/MembersComponent/MembersStart.js
@@ -4,11 +4,18 @@ export default function MemberStart(){
 
 
     return(
-        <div className=" MemberStartBg text justify-center flex items-center">
-        {" "}
-          
-        <h1 className="text-white text-6xl textMember  font-bold  text font-serif text-center">Meet Our <span className="text-[#4dff0c]">Team</span></h1>
-     
-      </div>
+
+<>
+<div className="hidden MemberStartBg  justify-center lg:flex items-center">
+{" "}
+
+<h1 className="text-white lg:text-6xl  textMember  font-bold  text font-serif text-center">Meet Our <span className="text-[#4dff0c]">Team</span></h1>
+</div>
+<div className="MemberMobile  h-[330px] lg:hidden text justify-center flex items-center">
+{" "}
+
+<h1 className="text-white lg:text-6xl sm:text-4xl text-4xl textMember  font-bold  text font-serif text-center">Meet Our <span className="text-[#4dff0c]">Team</span></h1>
+</div>
+</>
     );
 }
