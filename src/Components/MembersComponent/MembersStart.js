31d2d34ed84e594318110e7061d29653f020426d
Soumyadip Maity <22053029@kiit.ac.in>
2024-07-27 12:34:07 +0530

    chore: Update MembersStart component with responsive styling and lazy loading for images

--- src/Components/MembersComponent/MembersStart.js
+++ src/Components/MembersComponent/MembersStart.js
@@ -6,15 +6,15 @@ export default function MemberStart(){
     return(
 
 <>
-<div className="hidden MemberStartBg  justify-center lg:flex items-center">
+<div className="hidden  MemberStartBg  justify-center 2lg:h-[430px] xl:h-[480px]  2lg:flex items-center">
 {" "}
 
-<h1 className="text-white lg:text-6xl  textMember  font-bold  text font-serif text-center">Meet Our <span className="text-[#4dff0c]">Team</span></h1>
+<h1 className="text-white lg:text-6xl  textMember font-bold  text font-serif text-center">Meet Our <span className="text-[#4dff0c]">Team</span></h1>
 </div>
-<div className="MemberMobile  h-[330px] lg:hidden text justify-center flex items-center">
+<div className="MemberMobile  h-[330px] sm:h-[400px]   lg:h-[480px] 2lg:hidden text justify-center flex items-center">
 {" "}
 
-<h1 className="text-white lg:text-6xl sm:text-4xl text-4xl textMember  font-bold  text font-serif text-center">Meet Our <span className="text-[#4dff0c]">Team</span></h1>
+<h1 className="text-white lg:text-6xl sm:text-5xl text-4xl textMember  font-bold  text font-serif text-center">Meet Our <span className="text-[#4dff0c]">Team</span></h1>
 </div>
 </>
     );
